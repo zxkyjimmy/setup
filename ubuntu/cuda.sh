a7ff07f94a2e9727772ef232239f398e2d6bb41a
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-01-28 15:07:07 +0800

    feat(cuda): cdi description auto generates

--- ubuntu/cuda.sh
+++ ubuntu/cuda.sh
@@ -13,34 +13,34 @@ sudo apt install -y nvidia-container-toolkit
 sudo sed -i 's/^#no-cgroups = false/no-cgroups = true/;' /etc/nvidia-container-runtime/config.toml
 
 # sudo nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
-cat <<END | sudo tee /usr/local/bin/cdi.sh
-#!/bin/bash
+# cat <<END | sudo tee /usr/local/bin/cdi.sh
+# #!/bin/bash
 
-if [ ! -f /etc/cdi/nvidia.yaml ]; then
-  nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
-fi
+# if [ ! -f /etc/cdi/nvidia.yaml ]; then
+#   nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
+# fi
 
-crontab -u root -r
-rm -- \$0
-END
-sudo chmod +x /usr/local/bin/cdi.sh
-(sudo crontab -u root -l 2>/dev/null || true; echo "@reboot sleep 60; /usr/local/bin/cdi.sh") | sudo crontab -u root -
+# crontab -u root -r
+# rm -- \$0
+# END
+# sudo chmod +x /usr/local/bin/cdi.sh
+# (sudo crontab -u root -l 2>/dev/null || true; echo "@reboot sleep 60; /usr/local/bin/cdi.sh") | sudo crontab -u root -
 
-sudo mkdir -p /usr/share/containers/oci/hooks.d
-cat <<EOF | sudo tee /usr/share/containers/oci/hooks.d/oci-nvidia-hook.json
-{
-    "version": "1.0.0",
-    "hook": {
-        "path": "/usr/bin/nvidia-container-toolkit",
-        "args": ["nvidia-container-toolkit", "prestart"],
-        "env": [
-            "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-        ]
-    },
-    "when": {
-        "always": true,
-        "commands": [".*"]
-    },
-    "stages": ["prestart"]
-}
-EOF
+# sudo mkdir -p /usr/share/containers/oci/hooks.d
+# cat <<EOF | sudo tee /usr/share/containers/oci/hooks.d/oci-nvidia-hook.json
+# {
+#     "version": "1.0.0",
+#     "hook": {
+#         "path": "/usr/bin/nvidia-container-toolkit",
+#         "args": ["nvidia-container-toolkit", "prestart"],
+#         "env": [
+#             "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+#         ]
+#     },
+#     "when": {
+#         "always": true,
+#         "commands": [".*"]
+#     },
+#     "stages": ["prestart"]
+# }
+# EOF
