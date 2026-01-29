35a363e9797a9fe58f9f648ca1f88a93b390a4c0
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-01-29 14:39:49 +0800

    set Ptyxis font and size

--- ubuntu/tweakTerminal.sh
+++ ubuntu/tweakTerminal.sh
@@ -10,6 +10,14 @@ find -type f -name '*Windows*' -delete
 cp -r ${FONT_NAME} ~/.local/share/fonts
 fc-cache -f -v
 
-PROFILE_ID=$( gsettings get org.gnome.Terminal.ProfilesList default | xargs echo )
-dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/use-system-font false
-dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/font "'CaskaydiaCove Nerd Font 14'"
+# Set terminal font to CaskaydiaCove Nerd Font 14
+. /etc/os-release
+if dpkg --compare-versions "$VERSION_ID" ge "25.10"; then
+    gsettings set org.gnome.Ptyxis font-name "CaskaydiaCove Nerd Font 14"
+    gsettings set org.gnome.Ptyxis restore-window-size false
+    gsettings set org.gnome.Ptyxis window-size "(uint32 80, uint32 24)"
+else
+    PROFILE_ID=$( gsettings get org.gnome.Terminal.ProfilesList default | xargs echo )
+    dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/use-system-font false
+    dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/font "'CaskaydiaCove Nerd Font 14'"
+fi
