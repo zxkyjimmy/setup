0bd9c7a9afd960fe4eafcc169e94baa1ae482027
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-01-29 15:20:03 +0800

    Don't use system font

--- ubuntu/tweakTerminal.sh
+++ ubuntu/tweakTerminal.sh
@@ -13,9 +13,10 @@ fc-cache -f -v
 # Set terminal font to CaskaydiaCove Nerd Font 14
 . /etc/os-release
 if dpkg --compare-versions "$VERSION_ID" ge "25.10"; then
+    gsettings set org.gnome.Ptyxis use-system-font false
     gsettings set org.gnome.Ptyxis font-name "CaskaydiaCove Nerd Font 14"
-    gsettings set org.gnome.Ptyxis restore-window-size false
-    gsettings set org.gnome.Ptyxis window-size "(uint32 80, uint32 24)"
+    # gsettings set org.gnome.Ptyxis window-size "(uint32 80, uint32 24)"
+    # gsettings set org.gnome.Ptyxis restore-window-size false
 else
     PROFILE_ID=$( gsettings get org.gnome.Terminal.ProfilesList default | xargs echo )
     dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/use-system-font false
