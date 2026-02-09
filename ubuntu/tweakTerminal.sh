49e6e500029ca2d64e79075825b48bbddf2275ff
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-02-09 12:52:45 +0800

    set Ptyxis window size

--- ubuntu/tweakTerminal.sh
+++ ubuntu/tweakTerminal.sh
@@ -15,8 +15,8 @@ fc-cache -f -v
 if dpkg --compare-versions "$VERSION_ID" ge "25.10"; then
     gsettings set org.gnome.Ptyxis use-system-font false
     gsettings set org.gnome.Ptyxis font-name "CaskaydiaCove Nerd Font 14"
-    # gsettings set org.gnome.Ptyxis window-size "(uint32 80, uint32 24)"
-    # gsettings set org.gnome.Ptyxis restore-window-size false
+    gsettings set org.gnome.Ptyxis restore-window-size false
+    gsettings set org.gnome.Ptyxis window-size "(uint32 80, uint32 24)"
 else
     PROFILE_ID=$( gsettings get org.gnome.Terminal.ProfilesList default | xargs echo )
     dconf write /org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/use-system-font false
