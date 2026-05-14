6184093d2b97d537ded939ddb813c62f10cff6ad
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-05-14 16:55:15 +0800

    fix cuda install code

--- ubuntu/cuda.sh
+++ ubuntu/cuda.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-sudo apt install cuda-toolkit
+sudo apt install -y cuda-toolkit
 
 sudo apt update && sudo apt install -y --no-install-recommends \
   ca-certificates \
   curl \
   gnupg2
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 sudo apt update
 sudo apt install -y nvidia-container-toolkit
-  
+sudo sed -i 's/^#no-cgroups = false/no-cgroups = true/;' /etc/nvidia-container-runtime/config.toml
+
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
 # sudo apt update
