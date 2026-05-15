9bdf8b471755940e31b1c8f9597070a81d3e686e
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-05-15 14:06:14 +0800

    remove grub splash

--- ubuntu/cuda.sh
+++ ubuntu/cuda.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt install -y cuda-toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2604/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install -y cuda-drivers
 
 sudo apt update && sudo apt install -y --no-install-recommends \
   ca-certificates \
@@ -12,6 +15,8 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
 sudo apt update
 sudo apt install -y nvidia-container-toolkit
 sudo sed -i 's/^#no-cgroups = false/no-cgroups = true/;' /etc/nvidia-container-runtime/config.toml
+sudo sed -i.bak -E 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)\bsplash\b ?/\1/' /etc/default/grub
+sudo update-grub
 
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
